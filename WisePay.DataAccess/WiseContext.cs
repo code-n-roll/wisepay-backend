1e9469e245e361cdf761805e77d0e6448caff914
Vlad Klimenkov <vlad.klimenkoff97@gmail.com>
2017-10-08 01:24:40 +0300

    Create project structure.

---
+++ WisePay.DataAccess/WiseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WisePay.Entities;
+
+namespace WisePay.DataAccess
+{
+    public class WiseContext : DbContext
+    {
+        public WiseContext(DbContextOptions<WiseContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WisePay.Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WisePay.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
