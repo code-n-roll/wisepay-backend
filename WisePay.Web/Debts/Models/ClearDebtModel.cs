ba17a3826b00337fec93166920f6cdc0877b61de
Vlad Klimenkov <vlad.klimenkovv@gmail.com>
2018-01-04 23:46:02 +0300

    Add clear debts endpoint

---
+++ WisePay.Web/Debts/Models/ClearDebtModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WisePay.Web.Debts.Models
+{
+    public class ClearDebtModel
+    {
+        public int UserId { get; set; }
+    }
+}
